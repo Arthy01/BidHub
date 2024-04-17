e2d958b0184f0423ae68e70747e52ccdadf9e501
Ajax1907 <120017484+Ajax1907@users.noreply.github.com>
2024-04-17 11:43:54 +0200

    tabellen unbefüllt

--- src/main/java/de/hwrberlin/bidhub/controller/OverviewController.java
+++ src/main/java/de/hwrberlin/bidhub/controller/OverviewController.java
@@ -1,4 +1,27 @@
 package de.hwrberlin.bidhub.controller;
 
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class OverviewController {
+
+    @FXML
+    private TableView<?> kaufeTable;
+
+    @FXML
+    private TableView<?> verkaufeTable;
+
+    @FXML
+    public void initialize() {
+        // Sorgen Sie dafür, dass die Spalten sich dynamisch auf die gesamte Breite der TableView aufteilen
+        adjustColumnWidths(kaufeTable);
+        adjustColumnWidths(verkaufeTable);
+    }
+
+    private void adjustColumnWidths(TableView<?> tableView) {
+        ObservableList<? extends TableColumn<?, ?>> columns = tableView.getColumns();
+        columns.forEach(column -> column.prefWidthProperty().bind(tableView.widthProperty().divide(columns.size())));
+    }
 }
